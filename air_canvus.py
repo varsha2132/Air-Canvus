ce1162814954c9e977d6d68368bdeecc704ca3eb
KotrikeAmruthaVarsha <145061730+varsha2132@users.noreply.github.com>
2024-12-09 23:09:10 +0530

    Update air_canvus.py

--- air_canvus.py
+++ air_canvus.py
@@ -1,7 +1,20 @@
-import cv2
 import numpy as np
-import mediapipe as mp
+import cv2
 from collections import deque
+
+#default called trackbar function 
+def setValues(x):
+   print("")
+
+
+# Creating the trackbars needed for adjusting the marker colour
+cv2.namedWindow("Color detectors")
+cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180,setValues)
+cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255,setValues)
+cv2.createTrackbar("Upper Value", "Color detectors", 255, 255,setValues)
+cv2.createTrackbar("Lower Hue", "Color detectors", 64, 180,setValues)
+cv2.createTrackbar("Lower Saturation", "Color detectors", 72, 255,setValues)
+cv2.createTrackbar("Lower Value", "Color detectors", 49, 255,setValues)
 
 
 # Giving different arrays to handle colour points of different colour
@@ -9,7 +22,6 @@ bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
 ypoints = [deque(maxlen=1024)]
-
 
 # These indexes will be used to mark the points in particular arrays of specific colour
 blue_index = 0
@@ -26,86 +38,79 @@ colorIndex = 0
 # Here is code for Canvas setup
 paintWindow = np.zeros((471,636,3)) + 255
 paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), (255,0,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), (0,255,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), (0,0,255), 2)
-paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), (0,255,255), 2)
+paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), colors[0], -1)
+paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), colors[1], -1)
+paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), colors[2], -1)
+paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), colors[3], -1)
 
 cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
 
-# initialize mediapipe
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
-mpDraw = mp.solutions.drawing_utils
-
-
-# Initialize the webcam
+# Loading the default webcam of PC.
 cap = cv2.VideoCapture(0)
-ret = True
-while ret:
-    # Read each frame from the webcam
+
+# Keep looping
+while True:
+    # Reading the frame from the camera
     ret, frame = cap.read()
-
-    x, y, c = frame.shape
-
-    # Flip the frame vertically
+    #Flipping the frame to see same side of yours
     frame = cv2.flip(frame, 1)
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    frame = cv2.rectangle(frame, (40,1), (140,65), (0,0,0), 2)
-    frame = cv2.rectangle(frame, (160,1), (255,65), (255,0,0), 2)
-    frame = cv2.rectangle(frame, (275,1), (370,65), (0,255,0), 2)
-    frame = cv2.rectangle(frame, (390,1), (485,65), (0,0,255), 2)
-    frame = cv2.rectangle(frame, (505,1), (600,65), (0,255,255), 2)
-    cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    #frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-    # Get hand landmark prediction
-    result = hands.process(framergb)
-
-    # post process the result
-    if result.multi_hand_landmarks:
-        landmarks = []
-        for handslms in result.multi_hand_landmarks:
-            for lm in handslms.landmark:
-                # # print(id, lm)
-                # print(lm.x)
-                # print(lm.y)
-                lmx = int(lm.x * 640)
-                lmy = int(lm.y * 480)
-
-                landmarks.append([lmx, lmy])
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
-            # Drawing landmarks on frames
-            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-        fore_finger = (landmarks[8][0],landmarks[8][1])
-        center = fore_finger
-        thumb = (landmarks[4][0],landmarks[4][1])
-        cv2.circle(frame, center, 3, (0,255,0),-1)
-        print(center[1]-thumb[1])
-        if (thumb[1]-center[1]<30):
-            bpoints.append(deque(maxlen=512))
-            blue_index += 1
-            gpoints.append(deque(maxlen=512))
-            green_index += 1
-            rpoints.append(deque(maxlen=512))
-            red_index += 1
-            ypoints.append(deque(maxlen=512))
-            yellow_index += 1
+    u_hue = cv2.getTrackbarPos("Upper Hue", "Color detectors")
+    u_saturation = cv2.getTrackbarPos("Upper Saturation", "Color detectors")
+    u_value = cv2.getTrackbarPos("Upper Value", "Color detectors")
+    l_hue = cv2.getTrackbarPos("Lower Hue", "Color detectors")
+    l_saturation = cv2.getTrackbarPos("Lower Saturation", "Color detectors")
+    l_value = cv2.getTrackbarPos("Lower Value", "Color detectors")
+    Upper_hsv = np.array([u_hue,u_saturation,u_value])
+    Lower_hsv = np.array([l_hue,l_saturation,l_value])
 
-        elif center[1] <= 65:
+
+    # Adding the colour buttons to the live frame for colour access
+    frame = cv2.rectangle(frame, (40,1), (140,65), (122,122,122), -1)
+    frame = cv2.rectangle(frame, (160,1), (255,65), colors[0], -1)
+    frame = cv2.rectangle(frame, (275,1), (370,65), colors[1], -1)
+    frame = cv2.rectangle(frame, (390,1), (485,65), colors[2], -1)
+    frame = cv2.rectangle(frame, (505,1), (600,65), colors[3], -1)
+    cv2.putText(frame, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+
+    # Identifying the pointer by making its mask
+    Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
+    Mask = cv2.erode(Mask, kernel, iterations=1)
+    Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
+    Mask = cv2.dilate(Mask, kernel, iterations=1)
+
+    # Find contours for the pointer after idetifying it
+    cnts,_ = cv2.findContours(Mask.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+    center = None
+
+    # Ifthe contours are formed
+    if len(cnts) > 0:
+    	# sorting the contours to find biggest 
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        # Get the radius of the enclosing circle around the found contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        # Draw the circle around the contour
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # Calculating the center of the detected contour
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+        # Now checking if the user wants to click on any button above the screen 
+        if center[1] <= 65:
             if 40 <= center[0] <= 140: # Clear Button
                 bpoints = [deque(maxlen=512)]
                 gpoints = [deque(maxlen=512)]
@@ -146,13 +151,8 @@ while ret:
         ypoints.append(deque(maxlen=512))
         yellow_index += 1
 
-    # Draw lines of all the colors on the canvas and frame
+    # Draw lines of all the colors on the canvas and frame 
     points = [bpoints, gpoints, rpoints, ypoints]
-    # for j in range(len(points[0])):
-    #         for k in range(1, len(points[0][j])):
-    #             if points[0][j][k - 1] is None or points[0][j][k] is None:
-    #                 continue
-    #             cv2.line(paintWindow, points[0][j][k - 1], points[0][j][k], colors[0], 2)
     for i in range(len(points)):
         for j in range(len(points[i])):
             for k in range(1, len(points[i][j])):
@@ -161,12 +161,15 @@ while ret:
                 cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
                 cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
 
-    cv2.imshow("Output", frame) 
+    # Show all the windows
+    cv2.imshow("Tracking", frame)
     cv2.imshow("Paint", paintWindow)
+    cv2.imshow("mask",Mask)
 
-    if cv2.waitKey(1) == ord('q'):
+	# If the 'q' key is pressed then stop the application 
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# release the webcam and destroy all active windows
+# Release the camera and all resources
 cap.release()
 cv2.destroyAllWindows()
